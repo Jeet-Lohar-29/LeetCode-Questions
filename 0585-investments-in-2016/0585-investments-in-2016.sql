77d941f3110c2221e5a6f34795e69ea3abb4dd6d
Jeet Lohar <148797745+Jeet-Lohar-29@users.noreply.github.com>
2026-06-25 10:23:07 +0530

    Time: 702 ms (16.12%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,10 +1,15 @@
 SELECT 
     ROUND(SUM(tiv_2016), 2) AS tiv_2016
-FROM (
-    SELECT 
-        tiv_2016,
-        COUNT(*) OVER(PARTITION BY tiv_2015) AS cnt_2015,
-        COUNT(*) OVER(PARTITION BY lat, lon) AS cnt_location
-    FROM Insurance
-) t
-WHERE cnt_2015 > 1 AND cnt_location = 1;
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015 
+    FROM Insurance 
+    GROUP BY tiv_2015 
+    HAVING COUNT(*) > 1
+) 
+AND (lat, lon) IN (
+    SELECT lat, lon 
+    FROM Insurance 
+    GROUP BY lat, lon 
+    HAVING COUNT(*) = 1
+);
