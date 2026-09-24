de5bdba950b2700add5c8d9ed5f51a2781543caf
Jeet Lohar <148797745+Jeet-Lohar-29@users.noreply.github.com>
2026-09-24 15:46:29 +0530

    Time: 0 ms (100%), Space: 19.1 MB (90.87%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        if not nums:
+            return 0
+        
+        prev2 = 0
+        prev1 = 0
+        
+        for num in nums:
+            current = max(prev1, prev2 + num)
+            
+            prev2 = prev1
+            prev1 = current
+            
+        return prev1
